5eddb74776620d481f51380f35c0529b12b265ed
lgluiz1 <legalhints@gmail.com>
2025-04-28 17:25:25 -0300

    BASE

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+def main():
+    st.title('Calculadora')
+
+    # Cria duas colunas
+    col1, col2 = st.columns(2)
+
+    # Inputs em colunas separadas
+    with col1:
+        calendario_de = st.date_input('Inicio de')
+        st.write('Data inicial:', calendario_de)
+    with col2:
+        calendario_ate = st.date_input('Final até')
+        st.write('Data final:', calendario_ate)
+
+    # Valor Salario
+    salario = st.number_input('Salario', value=0, step=1)
+    st.write('Salario:', salario)
+
+
+    # Botão para calcular
+    if st.button('Calcular'):
+
+        st.write('Data inicial:', calendario_de)
+        st.write('Data final:', calendario_ate)
+    
+
+if __name__ == '__main__':
+    main()
